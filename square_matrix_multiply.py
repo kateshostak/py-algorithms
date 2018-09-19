4f16b4f83663dfa5693fc75c6893051b8b47c810
Kate Shostak <katherine.shostak@gmail.com>
2018-09-19 17:48:23 +0300

    Fix the matrix multiplication

--- square_matrix_multiply.py
+++ square_matrix_multiply.py
@@ -1,20 +1,26 @@
 import random
 
 def generate_matrix(n):
-    return [[random.randint(-100, 100) for i in range(n)] for j in range(n)]
+    return [[random.randint(0, 1) for i in range(n)] for j in range(n)]
 
 
 def multiply_matrices(a, b):
     c = list()
-    for i in range(len(a)):
-        tmp_elem = 0
-        for j in range(i):
-            tmp_elem += a[i][j]*b[j][i]
-            c.append(tmp_elem)
+    n = len(a)
+    for i in range(n):
+        tmp_row = list()
+        for j in range(n):
+            tmp_elem = 0
+            for k in range(n):
+                tmp_elem += a[i][k]*b[k][j]
+
+            tmp_row.append(tmp_elem)
+
+        c.append(tmp_row)
     return c
 
 def main():
-    n = 5
+    n = 3
     a = generate_matrix(n)
     b = generate_matrix(n)
     print(multiply_matrices(a, b))
