797c4ba2bd176c092a91790fe4e9abe8c7f8b879
Kate Shostak <katherine.shostak@gmail.com>
2018-10-10 18:00:19 +0300

    Fix the heapsort so it really takes O(nlgn) to sort an array

--- heapsort.py
+++ heapsort.py
@@ -2,22 +2,26 @@ import heap
 
 def heapsort(my_arr):
     sorted_arr = list()
+    my_heap = heap.Heap(my_arr)
     while len(my_arr) > 2:
-        my_heap = heap.Heap(my_arr)
         sorted_arr.append(my_heap.heap[0])
-        my_heap.heap[0] = my_heap.heap[my_heap.length - 1]
+        my_heap.heap[0] = my_heap.heap[-1]
         my_heap.heap.pop()
-        my_arr = my_heap.heap
+        my_heap.max_heapify(my_heap.heap, 0)
 
-    for elem in my_arr:
-        sorted_arr.append(max(my_arr))
+    if my_arr[0] > my_arr[1]:
+        sorted_arr.append(my_arr[0])
+        sorted_arr.append(my_arr[1])
+    else:
+        sorted_arr.append(my_arr[1])
+        sorted_arr.append(my_arr[0])
 
-    return sorted_arr
+    return sorted_arr[::-1]
 
 def main():
     sorted_arr = list()
     my_arr = [100, 99, 97, 95, 89, 80, 92, 77, 68, 77, 80, 33, 59, 49, 90, 50, 25, 35, 61, 68, 60, 69, 78, 30, 22, 36, 58, 13, 0, 53, 4, 4, 34, 6, 23, 32, 31, 56, 5, 34, 5, 1, 51, 12, 66, 35, 50, 0, 5]
-    print(heapsort(my_arr)[-1:0:-1])
+    print(heapsort(my_arr))
 
 if __name__ == '__main__':
     main()
