46fc80fd286a4920bee1be25f00adb98f099f8ce
Kate Shostak <katherine.shostak@gmail.com>
2018-10-12 16:13:21 +0100

    Make convinience changes so the heap object could be treated as an array

--- heap.py
+++ heap.py
@@ -3,8 +3,29 @@ import math
 class Heap():
 
     def __init__(self, arr):
-        self.length = len(arr)
+        # self.length = len(arr)
         self.heap = self.make_heap(arr)
+
+    def __iter__(self):
+        return iter(self.heap)
+
+    def __next__(self):
+        next(iter(self.heap))
+
+    def __getitem__(self, index):
+        return self.heap[index]
+
+    def __setitem__(self, index, value):
+        self.heap[index] = value
+
+    def __len__(self):
+        return len(self.heap)
+
+    def pop(self):
+        self.heap.pop()
+
+    def __str__(self):
+        return str(self.heap)
 
     def parent(self, i):
         return math.floor(i/2) - 1
@@ -16,37 +37,47 @@ class Heap():
         return 2*(i+1)
 
     def make_heap(self, arr):
-        for i in range(self.length//2, -1, -1):
+        length = len(arr)
+        for i in range(length//2, -1, -1):
             self.max_heapify(arr, i)
         return arr
 
     def max_heapify(self, arr, i):
-        left = self.left(i)
-        right = self.right(i)
+        if arr:
+            length = len(arr)
+            left = self.left(i)
+            right = self.right(i)
 
-        if i > self.length//2:
-            return
+            if i > length//2:
+                return
 
-        if left < self.length and arr[left] > arr[i]:
-            largest = left
-        else:
-            largest = i
+            if left < length and arr[left] > arr[i]:
+                largest = left
+            else:
+                largest = i
 
-        if  right < self.length and arr[right] > arr[largest]:
-            largest = right
+            if  right < length and arr[right] > arr[largest]:
+                largest = right
 
-        arr[i], arr[largest] = arr[largest], arr[i]
+            arr[i], arr[largest] = arr[largest], arr[i]
 
-        if largest != i:
-            self.max_heapify(arr, largest)
+            if largest != i:
+                self.max_heapify(arr, largest)
 
 
     def print_heap(self):
         print(self.heap)
 
+    def is_empty(self):
+        return not(self.heap)
+
+    def max_elem(self):
+        return self.heap[0]
+
+
 def main():
-    my_arr = [500, 23, 56, 57, 478, 479, 900]
-    # my_arr = [23, 17, 14, 6, 13, 10, 1, 5, 7, 12]
+    # my_arr = [500, 23, 56, 57, 478, 479, 900]
+    my_arr = [1]
     my_heap = Heap(my_arr)
     my_heap.print_heap()
 
