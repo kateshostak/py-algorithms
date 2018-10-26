3de7448fd10e207ebe4dc421e4c2db7d68b35212
Kate Shostak <katherine.shostak@gmail.com>
2018-10-26 16:32:09 +0100

    Add the test for improved bubblesort

---
+++ bublesort_improved_test.py
@@ -0,0 +1,20 @@
+import bublesort_improved
+import generate_random_arr
+
+def test_bublesort_improved(arr):
+    sorted_arr = sorted(arr)
+    if bublesort_improved.bublesort(arr) == sorted_arr:
+        return True
+
+
+def main():
+    arr = generate_random_arr.generate_arr(0, 9, 0, 100)
+    passed = True
+    for i in range(50):
+        passed = passed and test_bublesort_improved(arr)
+
+    if passed:
+        print('OK')
+
+if __name__ == '__main__':
+    main()
