c9a1b828afc9e30f7aba19f3453aea692dfc856b
Kate Shostak <katherine.shostak@gmail.com>
2018-10-30 17:29:31 +0100

    Add small fix to the test

--- bublesort_improved_test.py
+++ bublesort_improved_test.py
@@ -1,6 +1,7 @@
 import bublesort_improved
 import generate_random_arr
 
+
 def test_bublesort_improved(arr):
     sorted_arr = sorted(arr)
     if bublesort_improved.bublesort(arr) == sorted_arr:
@@ -11,6 +12,7 @@ def main():
     arr = generate_random_arr.generate_arr(0, 9, 0, 100)
     passed = True
     for i in range(50):
+        arr = generate_random_arr.generate_arr(0, 9, 0, 100)
         passed = passed and test_bublesort_improved(arr)
 
     if passed:
