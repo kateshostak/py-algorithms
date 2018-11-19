8a9febf729fe956e765f9343b2d317295ef0f3f5
Kate Shostak <katherine.shostak@gmail.com>
2018-11-19 09:08:35 +0100

    Simplify the solution to run in O(n)

--- minimum_swaps2.py
+++ minimum_swaps2.py
@@ -1,21 +1,13 @@
-def find_min(slice):
-    min_elem = slice[0]
-    min_i = 0
-    for i in range(len(slice)):
-        if slice[i] < min_elem:
-            min_elem = slice[i]
-            min_i = i
-    return min_i, min_elem
-
-
 def minimumSwaps(arr):
     swaps = 0
-    for i in range(len(arr) - 1):
-        j, elem = find_min(arr[i:])
-        if elem != arr[i]:
-            arr[i], arr[j+i] = arr[j+i], arr[i]
+    i = 0
+    while i < len(arr) - 1:
+        if arr[i] != i + 1:
+            j = arr[i] - 1
+            arr[i], arr[j] = arr[j], arr[i]
             swaps += 1
-    print(arr)
+        else:
+            i += 1
     return swaps
 
 
