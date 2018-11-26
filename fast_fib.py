cd08efef2430058f7d6e9ba1cd2a1e6f085be221
Kate Shostak <katherine.shostak@gmail.com>
2018-11-26 10:05:44 +0100

    Cut out the unnecessary list utilization and replace it with using only two variables

--- fast_fib.py
+++ fast_fib.py
@@ -1,19 +1,16 @@
-
 def fib(n):
     if n <= 1:
         return n
-    fib_seq = list()
-    fib_seq.append(0)
-    fib_seq.append(1)
-    for i in range(2, n + 1):
-        a = fib_seq[i-1] + fib_seq[i-2]
-        fib_seq.append(a)
-    return fib_seq[-1]
+    fib_1 = 0
+    fib_2 = 1
+    for i in range(n):
+        fib_1, fib_2 = fib_1 + fib_2, fib_1
+
+    return fib_1
 
 
 def main():
     n = int(input().strip())
     print(fib(n))
-
 if __name__ == '__main__':
      main()
