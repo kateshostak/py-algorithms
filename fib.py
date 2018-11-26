c3a410435064eba23ee35af937dd9875f2ecc0e9
Kate Shostak <katherine.shostak@gmail.com>
2018-11-26 10:25:34 +0100

    Fix the indentation

--- fib.py
+++ fib.py
@@ -1,13 +1,11 @@
 def fib(n):
-    if n == 0:
-        yield 0
-    elif n == 1:
-        yield 1
-    yield fib(n - 1) + fib(n - 2)
+    if n <= 1:
+        return n
+    return fib(n - 1) + fib(n - 2)
 
 
 def main():
-    n = 7
+    n = 100
     print(list(fib(n)))
 
 if __name__ == '__main__':
